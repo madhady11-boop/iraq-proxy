408b8d59018e22b80be61906bddd7a5a9619d912
madhady11-boop <madhady11@gmail.com>
2026-04-13 16:55:22 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,9 +1,8 @@
 import os
 
-# هذا الكود يثبت ويشغل سيرفر VLESS بسيط
-os.system("curl -L https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o v2ray.zip && unzip v2ray.zip")
+# كود سيرفر VMess احترافي للأيفون
 with open("config.json", "w") as f:
-    f.write('{"inbounds":[{"port":8443,"protocol":"vless","settings":{"clients":[{"id":"88888888-8888-8888-8888-888888888888"}],"decryption":"none"},"streamSettings":{"network":"ws"}}],"outbounds":[{"protocol":"freedom"}]}')
+    f.write('{"inbounds":[{"port":8443,"protocol":"vmess","settings":{"clients":[{"id":"88888888-8888-8888-8888-888888888888"}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/ahmed"}}}],"outbounds":[{"protocol":"freedom"}]}')
 
-print("🚀 VLESS SERVER IS READY")
-os.system("./v2ray run -c config.json")
+print("🚀 VMess SERVER IS READY FOR IPHONE")
+os.system("curl -L https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o v2ray.zip && unzip v2ray.zip && ./v2ray run -c config.json")
