e37c565a7f62a43a2088c0e84badaf3c13b849c0
madhady11-boop <madhady11@gmail.com>
2026-04-13 17:09:55 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,10 +1,9 @@
 import os
 
-# تحميل أداة MTProxy الاحترافية
-print("📥 Installing MTProxy...")
-os.system("curl -L https://github.com/9seconds/mtg/releases/latest/download/mtg-2.1.7-linux-amd64.tar.gz -o mtg.tar.gz && tar -xvf mtg.tar.gz")
+# كود يشغل سيرفر "تخفي" بورت 8443
+config = '{"inbounds":[{"port":8443,"protocol":"vmess","settings":{"clients":[{"id":"88888888-8888-8888-8888-888888888888"}]},"streamSettings":{"network":"ws"}}],"outbounds":[{"protocol":"freedom"}]}'
+with open("config.json", "w") as f: f.write(config)
 
-# تشغيل البروكسي (بورت 8443 مع سر سري)
-print("🚀 MTProto is Starting...")
-# السر هو: ee00000000000000000000000000000000676f6f676c652e636f6d
-os.system("./mtg-2.1.7-linux-amd64/mtg run ee00000000000000000000000000000000676f6f676c652e636f6d -b 0.0.0.0:8443")
+os.system("curl -L https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o v.zip && unzip v.zip")
+print("🚀 CLOAK SERVER READY")
+os.system("./v2ray run -c config.json")
