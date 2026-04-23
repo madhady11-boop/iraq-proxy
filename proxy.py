a4cf99b871e8c7c7c00297169640cf7da54b6cca
madhady11-boop <madhady11@gmail.com>
2026-04-23 16:38:48 +0300

    Delete proxy.py

--- proxy.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# كود يشغل سيرفر "تخفي" بورت 8443
-config = '{"inbounds":[{"port":8443,"protocol":"vmess","settings":{"clients":[{"id":"88888888-8888-8888-8888-888888888888"}]},"streamSettings":{"network":"ws"}}],"outbounds":[{"protocol":"freedom"}]}'
-with open("config.json", "w") as f: f.write(config)
-
-os.system("curl -L https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o v.zip && unzip v.zip")
-print("🚀 CLOAK SERVER READY")
-os.system("./v2ray run -c config.json")
